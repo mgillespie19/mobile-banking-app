5f104a8bcf5680d179a83231c6ad8b85debd6ce1
Max Gillespie <MaxGillespie@Maxs-MacBook-Pro.local>
2018-12-01 01:32:01 -0600

    page view working

--- cardable/views/mainScroll/mainPageView.swift
+++ cardable/views/mainScroll/mainPageView.swift
@@ -9,23 +9,18 @@
 import Foundation
 import UIKit
 
-//class mainScrollView: UIScrollView {
-//
-//}
-
 
 class PageViewController: UIPageViewController
 {
     fileprivate lazy var pages: [UIViewController] = {
         return [
-            self.getViewController(withIdentifier: "credit"),
+            self.getViewController(withIdentifier: "landing"),
             self.getViewController(withIdentifier: "debit"),
-            self.getViewController(withIdentifier: "landing")
+            self.getViewController(withIdentifier: "credit")
         ]
     }()
     
-    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController
-    {
+    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController {
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
     }
     
@@ -35,10 +30,17 @@ class PageViewController: UIPageViewController
         self.dataSource = self
         self.delegate   = self
         
+        
         if let firstVC = pages.first
         {
-            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+            setViewControllers([firstVC], direction: .reverse, animated: true, completion: nil)
         }
+    }
+    
+    func setupPageView () {
+        pages.append(creditView())
+        pages.append(debitView())
+        pages.append(landingView())
     }
 }
 
@@ -46,13 +48,28 @@ extension PageViewController: UIPageViewControllerDataSource
 {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         
+        
         guard let viewControllerIndex = pages.index(of: viewController) else { return nil }
+        
+        print(viewControllerIndex)
         
         let previousIndex = viewControllerIndex - 1
         
-        guard previousIndex >= 0          else { return pages.last }
+        guard previousIndex >= 0
+            else {
+                if previousIndex == -1 {
+                    return pages.last
+                }
+                else {
+                    return nil
+                }
+            }
         
-        guard pages.count > previousIndex else { return nil        }
+        if viewControllerIndex == 2 {
+            return nil
+        }
+        
+        guard pages.count - 1 > previousIndex else { return nil }
         
         return pages[previousIndex]
     }
@@ -63,9 +80,9 @@ extension PageViewController: UIPageViewControllerDataSource
         
         let nextIndex = viewControllerIndex + 1
         
-        guard nextIndex < pages.count else { return pages.first }
+       guard nextIndex < pages.count - 1 else { return nil }
         
-        guard pages.count > nextIndex else { return nil         }
+        guard pages.count - 1 > nextIndex else { return nil         }
         
         return pages[nextIndex]
     }
